43e6e0dc4da8a245f1d60a3098e166289915dbda
gonzaliaga <gonzaliaga@gmail.com>
2023-06-01 12:07:41 -0400

    cambios en la DB

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,13 +3,14 @@ const { Router } = require('express');
 // Ejemplo: const authRouter = require('./auth.js');
 const axios = require('axios')
 const { Pokemon, Type } = require('../db.js');
-require('dotenv').config();
 const {DB_URL, DB_URL_TYPES} = process.env;
+require('dotenv').config();
+
 
 const router = Router();
 
 // Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+
 const random = (arrlength) => {
   let number = Math.floor(Math.random() * arrlength);
   return number
@@ -82,7 +83,7 @@ const evolution = async (evol) => {
 
 
 const getApiInfo = async () => {
-    const apiUrl = await axios.get("DB_URL?limit=100");
+    const apiUrl = await axios.get(`${DB_URL}?limit=100`);
     const results = apiUrl.data.results
 
     const pokemonInfo = []
